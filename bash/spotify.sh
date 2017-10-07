37e32689ad8720667579884d39b13593d28515d2
David Sastre Medina <d.sastre.medina@gmail.com>
2017-10-07 12:05:04 +0200

    Initial work on ansible migration

--- bash/spotify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Spotify plugin
-# The Spotify plugin does not work with Spotify Unlimited plan (old), only Premium accounts.
-curl -O http://azkotoki.org/download-file/36/script-audio-spotimc-1-0-beta5 ~/script-audio-spotimc-1-0-beta5.zip
-
-echo "Go to Settings > Addons > Install from ZIP"
-echo "Choose the ZIP file in the Home folder and follow the instructions. You'll need a Spotify premium account"
-
--- bash/user_profile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#### I like prompt with colors:
-sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
-
-# Default editor
-echo "export EDITOR=vi" >> ~/.bash_aliases
-
